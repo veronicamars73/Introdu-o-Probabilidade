31e8fe4ade183f17a7c150a5d5ee8d3d3c8aa172
veronicamars73 <mendie73@gmail.com>
2020-07-29 12:43:31 -0300

    Definindo espaco amostral, casos favoraveis e contador

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 import numpy as np
+
+# Definiremos nosso espaço amostral como uma lista de todos os valores de um dado
+espaco_amostral = [1,2,3,4,5,6]
+
+# Definiremos nossos casos favoráveis como uma lista valores pares de um dado
+favoraveis = [2,4,6]
+
+# Definiremos nosso contador de lançamentos com resultados pares
+resultados_pares = 0

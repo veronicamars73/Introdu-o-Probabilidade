cddc2d2d9cc4d96c7bdc238ed677d1e7268ac23f
veronicamars73 <mendie73@gmail.com>
2020-07-29 13:04:06 -0300

    Adicionando um cálculo de probabilidade e uma constante lançamento

--- main.py
+++ main.py
@@ -9,8 +9,11 @@ favoraveis = [2,4,6]
 # Definiremos nosso contador de lançamentos com resultados pares
 resultados_pares = 0
 
+# Definiremos nosso número de lançamentos, iremos usar 10000 no nosso exemplo
+NUM_LANCAMENTOS = 10000
+
 # Agora escreveremos nosso iterador com nossos lançamentos
-for i in range (10000):
+for i in range (NUM_LANCAMENTOS):
 	# np.random.choice escolhe um dos valores no espaço amostral, ou seja, algum valor entre (1,2,3,4,5,6)
     x = np.random.choice(espaco_amostral)
     """O comando x in favoráveis checa se o valor escolhido
@@ -19,5 +22,8 @@ for i in range (10000):
     if(x in favoraveis):
         resultados_pares += 1
 
+# Definição da probabilidade
+probabilidade_par  = resultados_pares/NUM_LANCAMENTOS
+
 # Impressão do resultado obtido
-print("{0: d} dos resultados, isto é, obtemos uma probabilidade de aproximadamente {1:.2f}".format(resultados_pares, resultados_pares/10000))
+print("{0: d} dos resultados, isto é, obtemos uma probabilidade de aproximadamente {1:.2f}".format(resultados_pares, probabilidade_par))
